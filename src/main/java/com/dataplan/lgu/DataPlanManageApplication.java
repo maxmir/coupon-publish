5538fc35d97ba9958e561306a29483b5244eb8d8
Jongheon Kim <maxmirjh@gmail.com>
2020-09-11 14:04:35 +0900

    initial commit

---
+++ src/main/java/com/dataplan/lgu/DataPlanManageApplication.java
@@ -0,0 +1,15 @@
+package com.dataplan.lgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPlanManageApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataPlanManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dataplan/lgu/DataPlanManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataplan.lgu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPlanManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
