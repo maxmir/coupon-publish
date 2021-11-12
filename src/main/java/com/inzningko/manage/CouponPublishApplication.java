7b53fd03f9a722a3a41abd420263350d7d026210
Kim Jong-heon <maxmirjh@gmail.com>
2021-11-12 13:26:06 +0900

    Update CouponPublishApplication.java

--- src/main/java/com/inzningko/manage/CouponPublishApplication.java
+++ src/main/java/com/inzningko/manage/CouponPublishApplication.java
@@ -8,12 +8,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication(scanBasePackages = "com.inzningko.manage")
 @MapperScan(basePackages = "com.inzningko.manage.**.mapper")
-//public class CouponPublishApplication {
-//    public static void main(String[] args) {
-//        SpringApplication.run(CouponPublishApplication.class, args);
-//    }
-//}
+public class CouponPublishApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CouponPublishApplication.class, args);
+    }
+}
 
+/*
 public class CouponPublishApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(CouponPublishApplication.class);
@@ -24,3 +25,4 @@ public class CouponPublishApplication extends SpringBootServletInitializer {
         return application.sources(CouponPublishApplication.class);
     }
 }
+*/
